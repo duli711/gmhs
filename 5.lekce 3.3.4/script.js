22d94977f689dac242fad69454ab5394824652e9
duli711 <renata.kucakova@seznam.cz>
2026-03-13 10:10:16 +0100

    13.3.

--- 5.lekce 3.3.4/script.js
+++ 5.lekce 3.3.4/script.js
@@ -1,34 +1,34 @@
 let isLoggednIn = true;
-let isAdmin = true;
-
-if (isLoggednIn){
+let isAdmin = false;
+ 
+if (isLoggednIn) {
     console.log("Vítej na stránce");
-
-    if (isAdmin) {
-        console,log("Přístup do adminu povolen.")
+ 
+    if(isAdmin) {
+        console.log("Přístup do adminu povolen.")
     } else {
-        console.log("Jsi přihlášen jako uživatel")
+        console.log("Jsi přihlášen jako uživatel!")
     }
 } else {
     console.log("Nejsi přihlášený")
 }
-
-let age = 31;
+ 
+let age = 17;
 let hasDriverLicense = true;
-
-if (age >=18 && hasDriverLicense){
-    console.log("Můžeš řídit!")
+ 
+if (age >= 18 && hasDriverLicense) {
+    console.log("Může řídit!")
 } else {
     console.log("Nemůžeš řídit!")
 }
-
+ 
 let isHoliday = false;
 let isWeekend = true;
-
+ 
 if (isHoliday || isWeekend) {
-    console.log("Je čas relaxovat!");
-} else {
-    console.log("Musíme pracovat!")
-}
-
+    console.log("Je čas relaxovat");
+    } else {
+        console.log("Nemusíme pracovat!")
+    }
    
+ 
