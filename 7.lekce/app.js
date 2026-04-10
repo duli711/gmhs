b86e7103c0f8eaf0d9aa7a29f9fe2445739e4792
duli711 <renata.kucakova@seznam.cz>
2026-04-10 10:10:45 +0200

    10.4.

---
+++ 7.lekce/app.js
@@ -0,0 +1,33 @@
+let counter = 2;
+ 
+while (counter < 10) {
+    let message = counter
+    if (counter === 7) {
+       message = "7 je tady";
+    }
+    console.log(message)
+    counter++;
+}
+ 
+for (let i = 0; i < 10; i++) {
+    if(i === 5) continue;
+    console.log(i)
+}
+ 
+for (let i = 2; i <= 100; i++) {
+  if (i % 2 === 0 && i !== 50) {
+    console.log(i);
+  }
+}
+ 
+for (let i = 2; i <= 100; i++) {
+  if (i === 50) {
+    continue;
+  }
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+ 
+
+ 
